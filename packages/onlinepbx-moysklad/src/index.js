185fd6b193fd8f0811ca88898d44fabbb002e48c
Vitaliy Makeev <w.makeev@gmail.com>
2016-11-17 18:37:28 +0500

    Корректировка api

--- packages/onlinepbx-moysklad/src/index.js
+++ packages/onlinepbx-moysklad/src/index.js
@@ -5,8 +5,4 @@ const { Core } = require('scaleflow')
 const actionsRouter = require('./middlewares/actionsRouter')
 const callerName = require('./middlewares/callerName')
 
-module.exports = {
-  callerName,
-  actionsRouter,
-  onlinepbxMoysklad: Core.middleware(actionsRouter, callerName)
-}
+module.exports = Core.middleware(actionsRouter, callerName)
