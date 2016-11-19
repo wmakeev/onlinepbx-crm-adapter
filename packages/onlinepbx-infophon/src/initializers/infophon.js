e3c83cef576b8381e859fc22ad18329d1227813a
Vitaliy Makeev <w.makeev@gmail.com>
2016-11-19 15:36:39 +0500

    Исправление ошибки
    Обработка случая, когда `options` не указаны

--- packages/onlinepbx-infophon/src/initializers/infophon.js
+++ packages/onlinepbx-infophon/src/initializers/infophon.js
@@ -5,7 +5,7 @@ const infophonSearch = require('./districts/search')
 const DEFAULT_DISTRICT = process.env.INFOPHON_DEFAULT_DISTRICT || 'Центральный федеральный округ'
 
 module.exports = function infophonInitializer (options, { instance }) {
-  let defaultDistrict = (options.infophon || {}).defaultDistrict || DEFAULT_DISTRICT
+  let defaultDistrict = ((options || {}).infophon || {}).defaultDistrict || DEFAULT_DISTRICT
   instance.infophon = {
     search (phone, district) {
       return infophonSearch(phone, district || defaultDistrict)
