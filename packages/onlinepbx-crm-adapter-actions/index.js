e31747c01b01c1b864daab43acfff34356dfb222
Vitaliy Makeev <w.makeev@gmail.com>
2016-11-19 15:05:49 +0500

    Комментарии

--- packages/onlinepbx-crm-adapter-actions/index.js
+++ packages/onlinepbx-crm-adapter-actions/index.js
@@ -1,10 +1,27 @@
+// Addon - https://onlinepbx.atlassian.net/wiki/pages/viewpage.action?pageId=32841
+
 module.exports = {
+  /** Addon | Поиск карточки по номеру */
   CONTACT_INFO: 'CONTACT_INFO',
-  CALLER_NAME: 'CALLER_NAME',
+
+  /** Addon | Загрузка звонков */
   CALL_UPLOAD: 'CALL_UPLOAD',
+
+  /** Addon | Создание контакта */
   CREATE_CONTACT: 'CREATE_CONTACT',
+
+  /** Addon | Создание Лида */
   CREATE_LEAD: 'CREATE_LEAD',
+
+  /** Addon | Показать список пользователей */
   LIST_USERS: 'LIST_USERS',
+
+  /** Addon | Добавить комментарий к звонку */
   COMMENT_CALL: 'COMMENT_CALL',
-  HTTP_ACTION: 'HTTP_ACTION'
+
+  /** HTTP запрос */
+  HTTP_ACTION: 'HTTP_ACTION',
+
+  /** Adapter | Карточка клиента */
+  CALLER_NAME: 'CALLER_NAME'
 }
